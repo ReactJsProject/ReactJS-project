82537144cc1abedd7a8c54de879cb6c13b73c87e
arya <aryasuresh9876@gmail.com>
2022-06-15 22:47:52 +0530

    image styled

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './homepage.css'
 function Homepage() {
-  
+  const [file, setFile] = useState();
+    function handleChange(e) {
+        console.log(e.target.files);
+        setFile(URL.createObjectURL(e.target.files[0]));
+    }
+
+
+    
   return (
     <div className="section">
       <div className='container'>
@@ -9,15 +16,21 @@ function Homepage() {
         <h1 className="heading">Face Mask Detection</h1>
         <div className="text">Online Face Mask Detector - Free. Compatible with JPEG, JPG, PNG and more</div>
         <label>
-        <input accept="image/*" id="icon-button-file"
-        type="file" style={{ display: 'none'}} />
-        <a className="upload-button">
-          <div className="buttontext" >Upload Image</div> 
-          <div className="arrow-bubble">
-            <img src="https://assets-global.website-files.com/616e938268c8f02f94b2b53c/616e938268c8f03e4db2b839_Upload.svg" loading="lazy" alt=""></img>
-            <img src="https://assets-global.website-files.com/616e938268c8f02f94b2b53c/629de47db6e437795a105533_Icon.svg" loading="lazy"  alt="" className="livestreaming-condition"></img>
-          </div>
-        </a>
+        <div className="container-3">
+            <a className="upload-button">
+              <div className="buttontext" >Upload Image</div> 
+                <div className="arrow-bubble"> 
+                  <img src="https://assets-global.website-files.com/616e938268c8f02f94b2b53c/616e938268c8f03e4db2b839_Upload.svg" loading="lazy" alt=""></img>
+                  <img src="https://assets-global.website-files.com/616e938268c8f02f94b2b53c/629de47db6e437795a105533_Icon.svg" loading="lazy"  alt="" className="livestreaming-condition"></img>
+                </div> 
+            </a>
+            <input accept="image/*" id="icon-button-file"
+            type="file" onChange={handleChange} style={{ display: 'none'}} />
+            <div className="image">
+              <img src={file} />
+            </div>
+        </div>
+        
         </label>
       </div>
       <div className="container-2">
